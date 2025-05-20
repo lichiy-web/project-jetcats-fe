a1268bfe1227e7b55dc3aff833f8feec3ca38db5
Kateryna <koliesnikova.kateryna@gmail.com>
2025-05-21 02:21:03 +0300

    Logo component created

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,10 @@
+import css from './Logo.module.css';
+import logo from '../../assets/logo.svg';
+
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+      <img src={logo} alt="Spendy logo" className={css.logo} />
+  );
 };
 
 export default Logo;

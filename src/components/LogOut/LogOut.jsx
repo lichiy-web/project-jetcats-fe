07d8379bfe7a3228f43bf381bec374b440ba6703
Kateryna <koliesnikova.kateryna@gmail.com>
2025-05-21 02:17:36 +0300

    LogOut component for desktop created

--- src/components/LogOut/LogOut.jsx
+++ src/components/LogOut/LogOut.jsx
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+import LogoutModal from '../LogoutModal/LogoutModal.jsx';
+import { ReactComponent as ExitIcon } from '../../assets/exit.svg';
+import css from './LogOut.module.css';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../redux/auth/slectors.js';
+
 const LogOut = () => {
-  return <div>LogOut</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { name } = useSelector(selectUser);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <>
+      <div className={css.container}>
+        <span className={css.name}>{name}</span>
+        <span className={css.divider}></span>
+        <button className={css.exitBtn} onClick={openModal}>
+          <ExitIcon className={css.icon} />
+          <span className={css.text}>Exit</span>
+        </button>
+      </div>
+
+      {isModalOpen && <LogoutModal closeModal={closeModal} />}
+    </>
+  );
 };
 
 export default LogOut;

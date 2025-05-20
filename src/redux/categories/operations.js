b34af82534cbc760b85545b04334c874c284280b
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-05-20 13:29:39 +0300

    create redux file structure for categories

---
+++ src/redux/categories/operations.js
@@ -0,0 +1,42 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { appApi } from '../api/api';
+
+export const fetchCategories = createAsyncThunk(
+  'transactions/fetchAll',
+  async (signal, thunkAPI) => {
+    return appApi
+      .get('/transactions', { signal: signal })
+      .then(({ data }) => data)
+      .catch(error => thunkAPI.rejectWithValue(error.message));
+  }
+);
+
+export const addTransaction = createAsyncThunk(
+  'transactions/addTransaction',
+  async (transaction, thunkAPI) => {
+    return appApi
+      .post('/transactions', transaction)
+      .then(({ data }) => data)
+      .catch(error => thunkAPI.rejectWithValue(error.message));
+  }
+);
+
+export const deleteTransaction = createAsyncThunk(
+  'transactions/deleteTransaction',
+  async (transactionId, thunkAPI) => {
+    return appApi
+      .delete(`/transactions/${transactionId}`)
+      .then(({ data }) => data._id)
+      .catch(error => thunkAPI.rejectWithValue(error.message));
+  }
+);
+
+export const patchTransaction = createAsyncThunk(
+  'transactions/patchTransaction',
+  async ({ transactionId, transaction }, thunkAPI) => {
+    return appApi
+      .patch(`/transactions/${transactionId}`, transaction)
+      .then(({ data }) => data)
+      .catch(error => thunkAPI.rejectWithValue(error.message));
+  }
+);
---
+++ src/redux/categories/selectors.js
@@ -0,0 +1,13 @@
+// import { createSelector } from '@reduxjs/toolkit';
+// import { selectNameFilter } from '../filters/selectors';
+
+export const selectTransactions = state => state.transactions.items;
+export const selectLoading = state => state.transaction.loading;
+export const selectError = state => state.transactions.error;
+// export const selectFilteredContacts = createSelector(
+//   [selectTransactions, selectNameFilter],
+//   (contactList, searchQuery) =>
+//     contactList.filter(({ name }) =>
+//       name.trim().toLowerCase().includes(searchQuery.trim().toLowerCase())
+//     )
+// );

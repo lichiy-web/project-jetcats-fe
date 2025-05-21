9ced5b4203c638bfc1ea8b9b72cbb1f60d7d7b66
Stepannovv <stepannovv@gmail.com>
2025-05-21 12:59:48 +0200

    Update HomeTab.jsx

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -1,9 +1,25 @@
 import ButtonAddTransaction from '../ButtonAddTransaction/ButtonAddTransaction';
 import TransactionList from '../TransactionList/TransactionList';
+import ModalAddTransaction from '../ModalAddTransaction/ModalAddTransaction';
+import { useState } from 'react';
+import s from './HomeTab.module.css';
+// import { useDispatch } from 'react-redux';
+// import { fetchTransactions } from '../../redux/transactions/operations';
+// import { fetchCategories } from '../../redux/categories/operations';
 
 const HomeTab = () => {
+  // const dispatch = useDispatch();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // useEffect(() => {
+  //   dispatch(fetchTransactions());
+  //   dispatch(fetchCategories());
+  // }, [dispatch]);
+
+  const toggleModal = () => setIsModalOpen(prev => !prev);
+
   return (
-    <div>
+    <section className={s.homeTabContainer}>
       <TransactionList />
       <ButtonAddTransaction onClick={toggleModal} />
       {isModalOpen && <ModalAddTransaction onClose={toggleModal} />}

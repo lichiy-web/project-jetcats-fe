7909db98dead3cbc95fc9fce97562f20071cfa67
Arina Riabukha <arrin.uq@gmail.com>
2025-05-21 16:08:02 +0300

    siderBar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,27 +4,29 @@ import s from './Navigation.module.css';
 
 const Navigation = () => {
   return (
-    <div className={s.container}>
-      <NavLink
-        className={({ isActive }) => clsx(s.link, isActive && s.active)}
-        to="/"
-      >
-        Home
-      </NavLink>
-      <NavLink
-        className={({ isActive }) => clsx(s.link, isActive && s.active)}
-        to="/statistics"
-      >
-        Statistics
-      </NavLink>
-      <NavLink
-        className={({ isActive }) =>
-          clsx(s.link, isActive && s.active, s.mobileOnly)
-        }
-        to="/currency"
-      >
-        Currency
-      </NavLink>
+    <div className={s.wrapper}>
+      <div className={s.container}>
+        <NavLink
+          className={({ isActive }) => clsx(s.link, isActive && s.active)}
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => clsx(s.link, isActive && s.active)}
+          to="/statistics"
+        >
+          Statistics
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            clsx(s.link, isActive && s.active, s.mobileOnly)
+          }
+          to="/currency"
+        >
+          Currency
+        </NavLink>
+      </div>
     </div>
   );
 };

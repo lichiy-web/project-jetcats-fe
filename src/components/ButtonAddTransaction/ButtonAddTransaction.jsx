6e72e0d4ecb9557a5a616d18e334591bb4c8f334
Eugene Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-05-22 17:49:11 +0300

    add fix button handler

--- src/components/ButtonAddTransaction/ButtonAddTransaction.jsx
+++ src/components/ButtonAddTransaction/ButtonAddTransaction.jsx
@@ -1,9 +1,21 @@
 // import { Plus } from 'lucide-react';
+import { useEffect, useRef } from 'react';
 import s from './ButtonAddTransaction.module.css';
 
 const ButtonAddTransaction = ({ onClick }) => {
+  const btn = useRef(null);
+  useEffect(() => {
+    const btnOffset = 24;
+    const homeTab = btn.current.parentElement;
+    homeTab.addEventListener('scroll', () => {
+      const homeTabScrollTop = homeTab.scrollTop;
+      btn.current.style.bottom = btnOffset - homeTabScrollTop + 'px';
+      btn.current.style.right = btnOffset + 'px';
+    });
+  }, [btn]);
   return (
     <button
+      ref={btn}
       className={s.addTransactionButton}
       onClick={onClick}
       aria-label="Add transaction"

dcb0028457f9a46c5fe4448ce4d23198e50b13d6
Eugene Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-05-23 04:39:44 +0300

    fix ModalAddTransaction

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -3,32 +3,27 @@ import ModalAddTransaction from '../ModalAddTransaction/ModalAddTransaction';
 import ModalDeleteTransaction from '../ModalDeleteTransaction/ModalDeleteTransaction';
 import ModalEditTransaction from '../ModalEditTransaction/ModalEditTransaction';
 import TransactionList from '../TransactionList/TransactionList';
-import { useState } from 'react';
 import s from './HomeTab.module.css';
-// import { useDispatch } from 'react-redux';
+import { MODALS, toggleModal } from '../../redux/modals/slice';
+import { useDispatch } from 'react-redux';
 // import { fetchTransactions } from '../../redux/transactions/operations';
 // import { fetchCategories } from '../../redux/categories/operations';
 
 const HomeTab = () => {
-  // const dispatch = useDispatch();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  // useEffect(() => {
-  //   dispatch(fetchTransactions());
-  //   dispatch(fetchCategories());
-  // }, [dispatch]);
-
-  const toggleModal = () => setIsModalOpen(prev => !prev);
+  const dispatch = useDispatch();
+  const handleModals = modal => {
+    dispatch(toggleModal(modal));
+  };
 
   return (
     <>
       <section className={s.homeTabContainer}>
         <TransactionList />
-        <ButtonAddTransaction onClick={toggleModal} />
+        <ButtonAddTransaction onClick={() => handleModals(MODALS.add)} />
+        <ModalAddTransaction />
         <ModalDeleteTransaction />
         <ModalEditTransaction />
       </section>
-      {isModalOpen && <ModalAddTransaction onClose={toggleModal} />}
     </>
   );
 };

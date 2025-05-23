adba1e86055db5fe7e79695c6f0b446603b1b770
Eugene Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-05-23 04:46:25 +0300

    fix HomeTab and ButtonAddTransaction

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -4,22 +4,15 @@ import ModalDeleteTransaction from '../ModalDeleteTransaction/ModalDeleteTransac
 import ModalEditTransaction from '../ModalEditTransaction/ModalEditTransaction';
 import TransactionList from '../TransactionList/TransactionList';
 import s from './HomeTab.module.css';
-import { MODALS, toggleModal } from '../../redux/modals/slice';
-import { useDispatch } from 'react-redux';
 // import { fetchTransactions } from '../../redux/transactions/operations';
 // import { fetchCategories } from '../../redux/categories/operations';
 
 const HomeTab = () => {
-  const dispatch = useDispatch();
-  const handleModals = modal => {
-    dispatch(toggleModal(modal));
-  };
-
   return (
     <>
       <section className={s.homeTabContainer}>
         <TransactionList />
-        <ButtonAddTransaction onClick={() => handleModals(MODALS.add)} />
+        <ButtonAddTransaction />
         <ModalAddTransaction />
         <ModalDeleteTransaction />
         <ModalEditTransaction />

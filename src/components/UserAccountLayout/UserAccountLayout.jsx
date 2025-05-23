d43a1f26a89421b004099eaec014a6f6029898ff
Kateryna <koliesnikova.kateryna@gmail.com>
2025-05-23 20:30:38 +0300

    feat: added UserAccountLayout for desktop

--- src/components/UserAccountLayout/UserAccountLayout.jsx
+++ src/components/UserAccountLayout/UserAccountLayout.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
-// import HomeTab from '../HomeTab/HomeTab';
 import SideBar from '../SideBar/SideBar';
 import css from './UserAccountLayout.module.css';
 

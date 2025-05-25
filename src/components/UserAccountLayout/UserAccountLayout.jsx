513317f13baa55b6a03ab7595eaf9415bfc3ad7b
Kateryna <koliesnikova.kateryna@gmail.com>
2025-05-25 19:42:29 +0300

    for dev pulling

--- src/components/UserAccountLayout/UserAccountLayout.jsx
+++ src/components/UserAccountLayout/UserAccountLayout.jsx
@@ -6,10 +6,13 @@ import SideBar from '../SideBar/SideBar';
 import MobNavigation from '../MobNavigation/MobNavigation';
 import css from './UserAccountLayout.module.css';
 import Balance from '../Balance/Balance';
+import Navigation from '../Navigation/Navigation';
+import BalanceOverview from '../BalanceOverview/BalanceOverview';
 
 const UserAccountLayout = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
-  const isTabletOrAbove = useMediaQuery({ minWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1279 });
+  const isDesktop = useMediaQuery({ minWidth: 1280 });
 
   const location = useLocation();
   const isHomePage = location.pathname === '/';
@@ -18,7 +21,20 @@ const UserAccountLayout = () => {
     <div>
       <Header />
       <div className={css.pageWrapper}>
-        {isTabletOrAbove && <SideBar />}
+        {isDesktop && <SideBar />}
+        {isTablet && (
+          <div className={css.tabletWrapper}>
+            <div className={css.tabletSidebar}>
+              <div className={css.navigation}>
+                <Navigation />
+              </div>
+              <div className={css.balance}>
+                <Balance />
+              </div>
+            </div>
+            <BalanceOverview className={css.balanceOverview} />
+          </div>
+        )}
         {isMobile && (
           <>
             <MobNavigation /> {isHomePage && <Balance />}

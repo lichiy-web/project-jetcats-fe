39024fbe9abb23c8a6c771d05a8ff775dccdf172
Natalia Stan <popkids.ua@gmail.com>
2025-05-26 12:32:38 +0300

    add responsive style

--- src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -3,32 +3,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import { selectIsModalAddTransaction } from '../../redux/modals/selectors';
 import { toggleModal, MODALS } from '../../redux/modals/slice';
 import AddTransactionForm from '../AddTransactionForm/AddTransactionForm';
+import s from './ModalAddTransaction.module.css';
 
 Modal.setAppElement('#root');
-
-const customStyles = {
-  overlay: {
-    backdropFilter: 'blur(7px)',
-    background: 'rgba(53, 83, 89, 0.4)',
-  },
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    padding: '40px 65px 46px',
-    border: 'none',
-    borderRadius: '8px',
-    background: 'var(--main-light-green)',
-    boxShadow: '0 10px 25px rgba(0, 0, 0, 0.1)',
-  },
-};
 
 const ModalAddTransaction = () => {
   const isModalAddTransaction = useSelector(selectIsModalAddTransaction);
   const dispatch = useDispatch();
+
   const closeModal = () => {
     dispatch(toggleModal(MODALS.add));
   };
@@ -39,7 +21,8 @@ const ModalAddTransaction = () => {
       onRequestClose={closeModal}
       shouldCloseOnOverlayClick={true}
       shouldCloseOnEsc={true}
-      style={customStyles}
+      overlayClassName={s.modalOverlay}
+      className={s.modalContent}
       contentLabel="Add Transaction Modal"
     >
       <AddTransactionForm onClose={closeModal} />

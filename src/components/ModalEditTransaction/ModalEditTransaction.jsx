948c34b84b56cc2b32b7a455ac16019763912fa7
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-05-27 19:23:07 +0300

    fix handle close ModalEditTransaction

--- src/components/ModalEditTransaction/ModalEditTransaction.jsx
+++ src/components/ModalEditTransaction/ModalEditTransaction.jsx
@@ -1,21 +1,21 @@
 import Modal from 'react-modal';
-import { useDispatch } from 'react-redux';
-import { toggleModal, MODALS } from '../../redux/modals/slice';
+// import { useDispatch } from 'react-redux';
+// import { toggleModal, MODALS } from '../../redux/modals/slice';
 import EditTransactionForm from '../EditTransactionForm/EditTransactionForm';
 import s from './ModalEditTransaction.module.css';
 
 Modal.setAppElement('#root');
 
 const ModalEditTransaction = ({ isOpen, onClose, transaction }) => {
-  const dispatch = useDispatch();
-  const closeModal = () => {
-    dispatch(toggleModal(MODALS.edit));
-  };
+  // const dispatch = useDispatch();
+  // const closeModal = () => {
+  //   dispatch(toggleModal(MODALS.edit));
+  // };
 
   return (
     <Modal
       isOpen={isOpen}
-      onRequestClose={closeModal}
+      onRequestClose={onClose}
       shouldCloseOnOverlayClick={true}
       shouldCloseOnEsc={true}
       overlayClassName={s.modalOverlay}

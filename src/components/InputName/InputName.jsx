443483f483c8bd13156363469676c2e59c025b2c
JohnRealy <branderok@gmail.com>
2025-05-28 00:21:20 +0200

    input done!

--- src/components/InputName/InputName.jsx
+++ src/components/InputName/InputName.jsx
@@ -1,24 +1,30 @@
-import { Field, useField } from 'formik';
+import { ErrorMessage, Field, useField } from 'formik';
 import { ImUser } from 'react-icons/im';
-import css from './InputName.module.css';
+import clsx from 'clsx';
 
 const InputName = () => {
-  const [field, meta] = useField('name');
+  const [, meta] = useField('name');
+  const isError = meta.touched && meta.error;
 
   return (
-    <div className="inputLogWrapper">
-      <ImUser size={24} color="#081222" />
-      <Field
-        {...field}
-        type="text"
-        name="name"
-        placeholder="Name"
-        className="inputLog"
-        autoComplete="new-name"
-      />
-      {meta.touched && meta.error && (
-        <div className={css.error}>{meta.error}</div>
-      )}
+    <div className="fieldLogWrapper">
+      <label htmlFor="name" className="labelLog">
+        Email
+      </label>
+      <div
+        className={clsx('inputLogWrapper', isError && 'inputLogWrapperError')}
+      >
+        <ImUser size={24} color={isError ? '#b20202' : '#081222'} />
+        <Field
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Name"
+          className={clsx('inputLog', isError && 'inputLogError')}
+          autoComplete="new-name"
+        />
+      </div>
+      <ErrorMessage name="name" component="div" className="error" />
     </div>
   );
 };

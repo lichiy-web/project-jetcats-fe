df380d8173f065e6762532aaf61d1c623746b280
Kateryna <koliesnikova.kateryna@gmail.com>
2025-05-27 22:15:14 +0300

    fixed styles

--- src/components/UserAccountLayout/UserAccountLayout.jsx
+++ src/components/UserAccountLayout/UserAccountLayout.jsx
@@ -1,29 +1,46 @@
 import { Outlet } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
-import { useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 import Header from '../Header/Header';
 import SideBar from '../SideBar/SideBar';
 import css from './UserAccountLayout.module.css';
 import Balance from '../Balance/Balance';
 import Navigation from '../Navigation/Navigation';
+import BalanceOverview from '../BalanceOverview/BalanceOverview';
 
 const UserAccountLayout = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
-  const isTabletOrAbove = useMediaQuery({ minWidth: 768 });
+  // const isTabletOrAbove = useMediaQuery({ minWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1279 });
+  const isDesktop = useMediaQuery({ minWidth: 1280 });
 
-  const location = useLocation();
-  const isHomePage = location.pathname === '/';
+  // const location = useLocation();
+  // const isHomePage = location.pathname === '/';
 
   return (
     <div>
-      <Header />
+      <div className={css.header}>
+        <Header />
+      </div>
       <div className={css.pageWrapper}>
-        {isTabletOrAbove && <SideBar />}
-        {isMobile && (
-          <>
-            <Navigation /> {isHomePage && <Balance />}
-          </>
+        {isDesktop && <SideBar />}
+
+        {isTablet && (
+          <div className={css.tabletWrapper}>
+            <div className={css.tabletSidebar}>
+              <div className={css.navigation}>
+                <Navigation />
+              </div>
+              <div className={css.balance}>
+                <Balance />
+              </div>
+            </div>
+            <BalanceOverview className={css.balanceOverview} />
+          </div>
         )}
+
+        {isMobile && <SideBar />}
+
         <main className={css.mainContent}>
           <Outlet />
         </main>

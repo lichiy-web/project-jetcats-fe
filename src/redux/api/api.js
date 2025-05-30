0918a0468914972e6a9d55554d0154b36597e972
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-05-30 16:53:25 +0300

    fix api

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const appApiConfig = {
-  baseURL: 'https://project-jetcats-be-dev.onrender.com',
-  // baseURL: 'https://project-jetcats-be.onrender.com',
+  // baseURL: 'https://project-jetcats-be-dev.onrender.com',
+  baseURL: 'https://project-jetcats-be.onrender.com',
   // baseURL: 'http://localhost:3000',
   timeout: 4000,
   // withCredentials: true,

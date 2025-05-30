7ecf3edf0a4b9a90cd501bcf4f71d11d212a4ff5
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-05-30 16:47:05 +0300

    fix routes

--- src/components/App/PrivateRoute.jsx
+++ src/components/App/PrivateRoute.jsx
@@ -1,23 +1,26 @@
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import {
   selectIsAuthLoadding,
   selectIsLoggedIn,
-  selectIsRefreshing,
 } from '../../redux/auth/selectors';
 
 export const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
+  const location = useLocation();
+
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  // const isLoadding = useSelector();
-  const isRefreshing = useSelector(selectIsRefreshing);
   const isAuthLoadding = useSelector(selectIsAuthLoadding);
   if (isAuthLoadding) return null;
-  console.log('In PrivateRoute: ', {
-    isAuthLoadding,
-    isRefreshing,
-    isLoggedIn,
-    redirectTo,
-    Component,
-  });
-  return isLoggedIn ? Component : <Navigate to={redirectTo} />;
+  // console.log('In PrivateRoute: ', {
+  //   isAuthLoadding,
+  //   isRefreshing,
+  //   isLoggedIn,
+  //   redirectTo,
+  //   Component,
+  // });
+  return isLoggedIn ? (
+    Component
+  ) : (
+    <Navigate to={redirectTo} state={{ from: location }} replace />
+  );
 };
--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const appApiConfig = {
+  baseURL: 'https://project-jetcats-be-dev.onrender.com',
   // baseURL: 'https://project-jetcats-be.onrender.com',
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
   timeout: 4000,
-  withCredentials: true,
+  // withCredentials: true,
   headers: {
     // 'Access-Control-Allow-Origin': 'http://localhost:5173',
     // 'Content-Type': 'application/json',
